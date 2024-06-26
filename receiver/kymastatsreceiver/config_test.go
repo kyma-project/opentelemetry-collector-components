d2369d4751e7061a6163623d6b530eb87a4efafe
Hisar Balik <hisar.balik@sap.com>
2024-06-26 09:34:36 +0200

    add config unit tests

---
+++ receiver/kymastatsreceiver/config_test.go
@@ -0,0 +1,74 @@
+package kymastatsreceiver
+
+import (
+	"github.com/kyma-project/opentelemetry-collector-components/internal/k8sconfig"
+	"github.com/kyma-project/opentelemetry-collector-components/receiver/kymastatsreceiver/internal/metadata"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/confmap/confmaptest"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func TestLoadConfig(t *testing.T) {
+	t.Parallel()
+
+	cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config.yaml"))
+	require.NoError(t, err)
+
+	duration := 30 * time.Second
+
+	tests := []struct {
+		id          component.ID
+		expected    component.Config
+		expectedErr error
+	}{
+		{
+			id: component.NewIDWithName(metadata.Type, "default"),
+			expected: &Config{
+
+				CollectionInterval: duration,
+				APIConfig: k8sconfig.APIConfig{
+					AuthType: "serviceAccount",
+				},
+			},
+		},
+
+		{
+			id: component.NewIDWithName(metadata.Type, "k8s"),
+			expected: &Config{
+
+				CollectionInterval: duration,
+				APIConfig: k8sconfig.APIConfig{
+					AuthType: "kubeConfig",
+				},
+			},
+		},
+		{
+			id: component.NewIDWithName(metadata.Type, "custom"),
+			expected: &Config{
+
+				CollectionInterval: 10 * time.Second,
+				APIConfig: k8sconfig.APIConfig{
+					AuthType: "serviceAccount",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.id.String(), func(t *testing.T) {
+			factory := NewFactory()
+			cfg := factory.CreateDefaultConfig()
+
+			sub, err := cm.Sub(tt.id.String())
+			require.NoError(t, err)
+			require.NoError(t, component.UnmarshalConfig(sub, cfg))
+
+			assert.NoError(t, component.ValidateConfig(cfg))
+			assert.Equal(t, tt.expected, cfg)
+		})
+	}
+}
