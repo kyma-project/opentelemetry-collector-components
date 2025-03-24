1a3398800a6d989c77ba0663d97ef55f63ecc9d5
Rakesh Garimella <rakesh.garimella@sap.com>
2025-03-24 10:12:40 +0100

    fix

---
+++ processor/serviceenrichmentprocessor/service_enrichment_processor_logs_test.go
@@ -0,0 +1,63 @@
+package serviceenrichmentprocessor
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/pdata/plog"
+	"testing"
+)
+
+func TestProcessLogs(t *testing.T) {
+	tt := []struct {
+		name                string
+		logs                plog.Logs
+		expectedServiceName string
+	}{
+		{
+			name: "logs with service name not set",
+			logs: setLogs(map[string]string{
+				"kyma.kubernetes_io_app_name": "foo-k8s-io-app-name",
+			}),
+			expectedServiceName: "foo-k8s-io-app-name",
+		},
+		{
+			name: "logs with service name set to unknown_service",
+			logs: setLogs(map[string]string{
+				"service.name":                "unknown_service",
+				"kyma.kubernetes_io_app_name": "foo-k8s-io-app-name",
+			}),
+			expectedServiceName: "foo-k8s-io-app-name",
+		},
+		{
+			name: "logs with service name not set and deployment name set",
+			logs: setLogs(map[string]string{
+				"k8s.deployment.name": "foo-deployment-name",
+			}),
+			expectedServiceName: "foo-deployment-name",
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			sep := serviceEnrichmentProcessor{}
+			res, err := sep.processLogs(context.TODO(), tc.logs)
+			require.NoError(t, err)
+			for i := 0; i < res.ResourceLogs().Len(); i++ {
+				attr := res.ResourceLogs().At(i).Resource().Attributes()
+				svcName, ok := attr.Get("service.name")
+				require.True(t, ok)
+				require.Equal(t, tc.expectedServiceName, svcName.AsString())
+			}
+		})
+	}
+}
+
+func setLogs(attrs ...map[string]string) plog.Logs {
+	logs := plog.NewLogs()
+	for _, attr := range attrs {
+		resLogs := logs.ResourceLogs().AppendEmpty()
+		for k, v := range attr {
+			resLogs.Resource().Attributes().PutStr(k, v)
+		}
+	}
+	return logs
+}
--- processor/serviceenrichmentprocessor/service_enrichment_processor_test.go
+++ processor/serviceenrichmentprocessor/service_enrichment_processor_test.go
@@ -18,38 +18,70 @@ func TestFetchFirstAvailableServiceName(t *testing.T) {
 			expected: "unknown_service",
 		},
 		{
-			name:     "kyma.kubernetes_app_io_name is set",
-			attr:     setAttributes(map[string]string{"kyma.kubernetes_io_app_name": "foo-io-app-name"}),
-			expected: "foo-io-app-name",
+			name: "kyma.kubernetes_app_io_name is set",
+			attr: setAttributes(map[string]string{
+				"kyma.kubernetes_io_app_name": "foo-k8s-io-app-name",
+			}),
+			expected: "foo-k8s-io-app-name",
 		},
 		{
-			name:     "kyma.app_name is set",
-			attr:     setAttributes(map[string]string{"kyma.app_name": "foo-app-name"}),
+			name: "when multiple attributes are set, the first one is returned",
+			attr: setAttributes(
+				map[string]string{
+					"kyma.kubernetes_io_app_name": "foo-k8s-io-app-name",
+					"kyma.app_name":               "foo-app-name",
+				}),
+			expected: "foo-k8s-io-app-name",
+		},
+		{
+			name: "kyma.app_name is set",
+			attr: setAttributes(map[string]string{
+				"kyma.app_name": "foo-app-name",
+			}),
 			expected: "foo-app-name",
 		},
 		{
-			name:     "k8s.deployment_name is set",
-			attr:     setAttributes(map[string]string{"k8s.deployment.name": "foo-deployment-name"}),
+			name: "when multiple attributes are set, if app_name and deployment_name are set, app_name is returned",
+			attr: setAttributes(
+				map[string]string{
+					"kyma.app_name":       "foo-app-name",
+					"k8s.deployment.name": "foo-deployment-name",
+				}),
+			expected: "foo-app-name",
+		},
+		{
+			name: "k8s.deployment_name is set",
+			attr: setAttributes(map[string]string{
+				"k8s.deployment.name": "foo-deployment-name",
+			}),
 			expected: "foo-deployment-name",
 		},
 		{
-			name:     "k8s.daemonset_name is set",
-			attr:     setAttributes(map[string]string{"k8s.daemonset.name": "foo-daemonset-name"}),
+			name: "k8s.daemonset_name is set",
+			attr: setAttributes(map[string]string{
+				"k8s.daemonset.name": "foo-daemonset-name",
+			}),
 			expected: "foo-daemonset-name",
 		},
 		{
-			name:     "k8s.statefulset_name is set",
-			attr:     setAttributes(map[string]string{"k8s.statefulset.name": "foo-statefulset-name"}),
+			name: "k8s.statefulset_name is set",
+			attr: setAttributes(map[string]string{
+				"k8s.statefulset.name": "foo-statefulset-name",
+			}),
 			expected: "foo-statefulset-name",
 		},
 		{
-			name:     "k8s.job_name is set",
-			attr:     setAttributes(map[string]string{"k8s.job.name": "foo-job-name"}),
+			name: "k8s.job_name is set",
+			attr: setAttributes(map[string]string{
+				"k8s.job.name": "foo-job-name",
+			}),
 			expected: "foo-job-name",
 		},
 		{
-			name:     "k8s.pod_name is set",
-			attr:     setAttributes(map[string]string{"k8s.pod.name": "foo-pod-name"}),
+			name: "k8s.pod_name is set",
+			attr: setAttributes(map[string]string{
+				"k8s.pod.name": "foo-pod-name",
+			}),
 			expected: "foo-pod-name",
 		},
 	}
@@ -61,6 +93,63 @@ func TestFetchFirstAvailableServiceName(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestSetServiceName(t *testing.T) {
+	tt := []struct {
+		name     string
+		attr     pcommon.Map
+		expected string
+	}{
+		{
+			name: "service name is set",
+			attr: setAttributes(map[string]string{
+				"service.name": "foo-service-name",
+			}),
+			expected: "foo-service-name",
+		},
+		{
+			name: "service name is set and deployment name is set",
+			attr: setAttributes(map[string]string{
+				"service.name":        "foo-service-name",
+				"k8s.deployment.name": "foo-deployment-name",
+			}),
+			expected: "foo-service-name",
+		},
+		{
+			name: "service name is unknown but pod name is set",
+			attr: setAttributes(map[string]string{
+				"k8s.pod.name": "foo-pod-name",
+			}),
+			expected: "foo-pod-name",
+		},
+		{
+			name: "service name is unset and app name is set",
+			attr: setAttributes(map[string]string{
+				"kyma.app_name": "foo-app-name",
+			}),
+			expected: "foo-app-name",
+		},
+		{
+			name: "service name is empty and job name is set",
+			attr: setAttributes(map[string]string{
+				"service.name": "",
+				"k8s.job.name": "foo-job-name",
+			}),
+			expected: "foo-job-name",
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			sep := serviceEnrichmentProcessor{}
+			sep.setServiceName(tc.attr)
+			got, _ := tc.attr.Get("service.name")
+			if got.AsString() != tc.expected {
+				t.Errorf("expected %s, got %s", tc.expected, got.AsString())
+			}
+		})
+	}
+
 }
 
 func setAttributes(attrs ...map[string]string) pcommon.Map {
