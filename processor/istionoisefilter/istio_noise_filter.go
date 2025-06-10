8a15ac28323d7389a8a4479f6470c15c9c1c0820
Stanislav Khalash <stanislav.khalash@sap.com>
2025-06-10 11:01:12 +0200

    Fix after CR

--- processor/istionoisefilter/istio_noise_filter.go
+++ processor/istionoisefilter/istio_noise_filter.go
@@ -24,16 +24,11 @@ func newProcessor(cfg *Config, set processor.Settings) *istioNoiseFilter {
 	}
 }
 
-//nolint:dupl //all 3 process methods are similar, but operate on different data types
 func (f *istioNoiseFilter) processTraces(_ context.Context, traces ptrace.Traces) (ptrace.Traces, error) {
-	for i := range traces.ResourceSpans().Len() {
-		resourceSpans := traces.ResourceSpans().At(i)
-
-		for j := range resourceSpans.ScopeSpans().Len() {
-			scopeSpans := resourceSpans.ScopeSpans().At(j)
-
-			scopeSpans.Spans().RemoveIf(func(span ptrace.Span) bool {
-				return rules.ShouldDropSpan(span, resourceSpans.Resource().Attributes())
+	for _, rs := range traces.ResourceSpans().All() {
+		for _, ss := range rs.ScopeSpans().All() {
+			ss.Spans().RemoveIf(func(span ptrace.Span) bool {
+				return rules.ShouldDropSpan(span, rs.Resource().Attributes())
 			})
 		}
 	}
@@ -41,16 +36,11 @@ func (f *istioNoiseFilter) processTraces(_ context.Context, traces ptrace.Traces
 	return traces, nil
 }
 
-//nolint:dupl //all 3 process methods are similar, but operate on different data types
 func (f *istioNoiseFilter) processLogs(_ context.Context, logs plog.Logs) (plog.Logs, error) {
-	for i := range logs.ResourceLogs().Len() {
-		resourceLogs := logs.ResourceLogs().At(i)
-
-		for j := range resourceLogs.ScopeLogs().Len() {
-			scopeLogs := resourceLogs.ScopeLogs().At(j)
-
-			scopeLogs.LogRecords().RemoveIf(func(logRecord plog.LogRecord) bool {
-				return rules.ShouldDropLogRecord(logRecord, resourceLogs.Resource().Attributes())
+	for _, rl := range logs.ResourceLogs().All() {
+		for _, sl := range rl.ScopeLogs().All() {
+			sl.LogRecords().RemoveIf(func(logRecord plog.LogRecord) bool {
+				return rules.ShouldDropLogRecord(logRecord, rl.Resource().Attributes())
 			})
 		}
 	}
@@ -58,43 +48,35 @@ func (f *istioNoiseFilter) processLogs(_ context.Context, logs plog.Logs) (plog.
 	return logs, nil
 }
 
-//nolint:dupl //all 3 process methods are similar, but operate on different data types
 func (f *istioNoiseFilter) processMetrics(_ context.Context, metrics pmetric.Metrics) (pmetric.Metrics, error) {
-	for i := range metrics.ResourceMetrics().Len() {
-		resourceMetrics := metrics.ResourceMetrics().At(i)
-
-		for j := range resourceMetrics.ScopeMetrics().Len() {
-			scopeMetrics := resourceMetrics.ScopeMetrics().At(j)
-
-			for k := range scopeMetrics.Metrics().Len() {
-				metric := scopeMetrics.Metrics().At(k)
-				metricName := metric.Name()
-
-				switch metric.Type() {
+	for _, rm := range metrics.ResourceMetrics().All() {
+		for _, sm := range rm.ScopeMetrics().All() {
+			for _, m := range sm.Metrics().All() {
+				switch m.Type() {
 				case pmetric.MetricTypeGauge:
-					metric.Gauge().DataPoints().RemoveIf(func(ndp pmetric.NumberDataPoint) bool {
-						return rules.ShouldDropMetricDataPoint(metricName, ndp.Attributes())
+					m.Gauge().DataPoints().RemoveIf(func(ndp pmetric.NumberDataPoint) bool {
+						return rules.ShouldDropMetricDataPoint(m.Name(), ndp.Attributes())
 					})
 				case pmetric.MetricTypeSum:
-					metric.Sum().DataPoints().RemoveIf(func(ndp pmetric.NumberDataPoint) bool {
-						return rules.ShouldDropMetricDataPoint(metricName, ndp.Attributes())
+					m.Sum().DataPoints().RemoveIf(func(ndp pmetric.NumberDataPoint) bool {
+						return rules.ShouldDropMetricDataPoint(m.Name(), ndp.Attributes())
 					})
 				case pmetric.MetricTypeHistogram:
-					metric.Histogram().DataPoints().RemoveIf(func(hdp pmetric.HistogramDataPoint) bool {
-						return rules.ShouldDropMetricDataPoint(metricName, hdp.Attributes())
+					m.Histogram().DataPoints().RemoveIf(func(hdp pmetric.HistogramDataPoint) bool {
+						return rules.ShouldDropMetricDataPoint(m.Name(), hdp.Attributes())
 					})
 				case pmetric.MetricTypeExponentialHistogram:
-					metric.ExponentialHistogram().DataPoints().RemoveIf(func(ehdp pmetric.ExponentialHistogramDataPoint) bool {
-						return rules.ShouldDropMetricDataPoint(metricName, ehdp.Attributes())
+					m.ExponentialHistogram().DataPoints().RemoveIf(func(ehdp pmetric.ExponentialHistogramDataPoint) bool {
+						return rules.ShouldDropMetricDataPoint(m.Name(), ehdp.Attributes())
 					})
 				case pmetric.MetricTypeSummary:
-					metric.Summary().DataPoints().RemoveIf(func(sdp pmetric.SummaryDataPoint) bool {
-						return rules.ShouldDropMetricDataPoint(metricName, sdp.Attributes())
+					m.Summary().DataPoints().RemoveIf(func(sdp pmetric.SummaryDataPoint) bool {
+						return rules.ShouldDropMetricDataPoint(m.Name(), sdp.Attributes())
 					})
 				default:
 					f.logger.Warn("Unknown metric type encountered in processMetrics",
-						zap.String("metric_name", metricName),
-						zap.Any("metric_type", metric.Type()),
+						zap.String("metric_name", m.Name()),
+						zap.Any("metric_type", m.Type()),
 					)
 				}
 			}
