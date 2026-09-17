d08a53db3a35409cc33bcbe9a579f1efd02a6a87
Teodor-Adrian Mihaescu <teodor-adrian.mihaescu@sap.com>
2026-09-17 15:51:02 +0200

    Bump OTel to v0.161.0/1.67.0

--- receiver/dummyreceiver/internal/metadata/generated_config.go
+++ receiver/dummyreceiver/internal/metadata/generated_config.go
@@ -123,8 +123,3 @@ func NewDefaultMetricsBuilderConfig() MetricsBuilderConfig {
 		ResourceAttributes: DefaultResourceAttributesConfig(),
 	}
 }
-
-// Deprecated: Use NewDefaultMetricsBuilderConfig.
-func DefaultMetricsBuilderConfig() MetricsBuilderConfig {
-	return NewDefaultMetricsBuilderConfig()
-}
